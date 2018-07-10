cbe26fb6d772c06da2daa18975c6c54e6f6a9410
mahiarmahjoub <mahiarmahjoub@gmail.com>
2018-07-10 17:34:47 +0100

    mosrgh

--- function_geneexp_ordering_clusters.R
+++ function_geneexp_ordering_clusters.R
@@ -4,10 +4,24 @@
 # future plan: order genes OR clusters of genes based of peak expression time
 
 
-temporal.order.genes <- function(x, order.fun=which.max){
+temporal.order.genes <- function(x){
   
-  gene.order <- order(apply(x, 1, order.fun))
+  peak.time <- apply(x, 1, which.max)
+  gene.order <- order(peak.time, decreasing = FALSE)
+  gene.order.names <- rownames(x[gene.order,])
   
-  return(list(peak.order=gene.order))
+  return(list(peak.time=peak.time, peak.order=gene.order, 
+              names=gene.order.names))
   
 }
+
+
+# gene.clusters <- hclust(dist((expr.selected)))
+# plot(gene.clusters)
+# gene.clusterCut <- cutree(gene.clusters, 500)
+# 
+# mean.time.peak.clust <- rep(NA,8)
+# for (i in 1:length(mean.time.peak.clust)){
+#   clust.members <- names(gene.clusterCut[gene.clusterCut==i])
+#   mean.time.peak.clust[i] <- mean(apply(expr.selected[clust.members,],1,which.max))
+# }
